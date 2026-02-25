7db0c75649c3bcdffc253756dbc27cb103580d63
devlucas-java <1uc4sm4c3d00@gmail.com>
2026-02-25 22:40:47 +0000

    feat: add delete handler logic

--- handler/deleteHandler.go
+++ handler/deleteHandler.go
@@ -1,13 +1,33 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/devlucas-java/api-opportunities/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func DeleteHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusNoContent, gin.H{
-		"message": "201 No Content",
-	})
+
+	id := ctx.Query("id")
+	if id == "" {
+		SendErrorResponse(
+			ctx,
+			http.StatusBadRequest,
+			errParamRequerided("id", "string").Error())
+		return
+	}
+
+	opening := schemas.Opening{}
+	if err := db.First(&opening, id).Error; err != nil {
+		SendErrorResponse(ctx, http.StatusNotFound, fmt.Sprintf("Opening it id: %v not found", id))
+		return
+	}
+
+	if err := db.Delete(&opening, id).Error; err != nil {
+		SendErrorResponse(ctx, http.StatusInternalServerError, fmt.Sprintf("Error delete opening id: %v", id))
+		return
+	}
+	SendSuccessResponse(ctx, fmt.Sprintf("delete opening id: %v"+id), opening)
 }
