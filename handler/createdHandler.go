c90a39efb92bd874eb6feda2b9c7f797cfb47fe3
devlucas-java <1uc4sm4c3d00@gmail.com>
2026-02-25 22:40:26 +0000

    feat: add created handler logic

--- handler/createdHandler.go
+++ handler/createdHandler.go
@@ -3,11 +3,36 @@ package handler
 import (
 	"net/http"
 
+	"github.com/devlucas-java/api-opportunities/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func CreatedHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "200 Created",
-	})
+
+	request := CreatedOpeningRequest{}
+
+	ctx.BindJSON(&request)
+
+	if err := request.Validate(); err != nil {
+		logger.Errorf("Error in validate request: %v", err.Error())
+		SendErrorResponse(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	opening := schemas.Opening{
+		Role:     request.Role,
+		Company:  request.Company,
+		Remote:   *request.Remote,
+		Salary:   request.Salary,
+		Location: request.Location,
+		Link:     request.Link,
+	}
+
+	if err := db.Create(&opening).Error; err != nil {
+		logger.Errorf("Error in create record: %v", err.Error())
+		SendErrorResponse(ctx, http.StatusInternalServerError, "Error in create record")
+		return
+	}
+
+	SendSuccessResponse(ctx, "created-opening", opening)
 }
