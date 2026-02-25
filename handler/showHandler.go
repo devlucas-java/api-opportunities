2f6bf76f3f691753cfff3bc5d29315e9910dfea0
devlucas-java <1uc4sm4c3d00@gmail.com>
2026-02-25 22:42:09 +0000

    feat: add show handler logic

--- handler/showHandler.go
+++ handler/showHandler.go
@@ -3,11 +3,25 @@ package handler
 import (
 	"net/http"
 
+	"github.com/devlucas-java/api-opportunities/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func ShowHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "200",
-	})
+
+	opening := schemas.Opening{}
+
+	id := ctx.Query("id")
+	if id == "" {
+		SendErrorResponse(
+			ctx,
+			http.StatusBadRequest,
+			errParamRequerided("id", "string").Error())
+		return
+	}
+	if err := db.Find(&opening, id).Error; err != nil {
+		SendErrorResponse(ctx, http.StatusNotFound, err.Error())
+		return
+	}
+	SendSuccessResponse(ctx, "show-opening", opening)
 }
